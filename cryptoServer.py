2283fa985254e6c9f81a99465df77ec1fcc62c7b
Jonathan Esteban <jesteban@mit.edu>
2020-05-02 02:43:14 -0400

    added user interface scaffolding

--- cryptoServer.py
+++ cryptoServer.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
+
+#set debug; setting to true allows for hot reload (automatic code deployment)
+DEBUG = False
+
+#instantiate app
 app = Flask(__name__)
+
+#enable CORS, used to communicate with UI server
+#allows cross-origin requests on all routes, from any domain, protocol, or port
+#TODO: secure for production!!
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    #runs the server
+    app.run(debug=DEBUG)
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 export FLASK_APP=cryptoServer.py
 flask run
+npx
