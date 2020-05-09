e8876f8dab71d5d01c5daeae39598a8657bfa904
Jonathan Esteban <jesteban@mit.edu>
2020-05-09 04:29:38 -0400

    recovered some old code

--- web-interface/src/App.vue
+++ web-interface/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     Home. {{ key }}
+    <br />
+    <v-button id="download_link" download="keypair.txt" href=""> Download keypair as text file</v-button>
   </div>
 </template>
 
@@ -13,7 +15,11 @@ export default {
       key: "key not yet updated."
     };
   },
-  created: function() {
+  mounted: function() {
+    var text = "Public Key: 43242344 \nSecret Key: 20348932805";
+    var data = new Blob([text], { type: "text/plain" });
+    var url = window.URL.createObjectURL(data);
+    document.getElementById("download_link").href = url;
     axios
       .get("http://127.0.0.1:5000/key")
       .then(response => {
